61cc84e57dea4def9700ffa0adcb7d92deaee4c4
lenny_cheng <langrisser1981@gmail.com>
2022-12-06 09:29:19 +0800

    Initial Commit

---
+++ KeyChainHelper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  KeyChainHelper
+//
+//  Created by 程信傑 on 2022/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeyChainHelper/KeyChainHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyChainHelperApp.swift
+//  KeyChainHelper
+//
+//  Created by 程信傑 on 2022/12/6.
+//
+
+import SwiftUI
+
+@main
+struct KeyChainHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
